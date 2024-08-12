8b2a6e170ac026ead1b3d5b298eccdbca41394ae
Camila <camila@railslove.com>
2024-08-12 13:00:29 +0200

    packages updated: storybook, jest, react-tooltip

--- src/components/mobile-screen/mobile-screen.stories.jsx
+++ src/components/mobile-screen/mobile-screen.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MobileScreen from './mobile-screen.jsx'
+import { MemoryRouter } from 'react-router-dom'
 
 export default {
   title: 'MobileScreen',
@@ -9,6 +10,11 @@ export default {
       <div style={{ margin: 'auto', maxWidth: '320px' }}>
         <WrappedStory />
       </div>
+    ),
+    (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <Story />
+      </MemoryRouter>
     ),
   ],
 }
